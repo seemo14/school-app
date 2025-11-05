39c107c24e0689999a2cc7c235845a0d1a1f0290
SeemoElon <37927509+seemo14@users.noreply.github.com>
2025-11-05 23:22:19 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <App />
-  </StrictMode>,
+  </BrowserRouter>
 )
