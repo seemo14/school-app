ccdbe64b6a78565332c4aa72a922e9b4cdd53017
SeemoElon <37927509+seemo14@users.noreply.github.com>
2025-11-06 01:24:34 +0100

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import path from 'node:path'
 
+// IMPORTANT: This makes GitHub Pages work at https://seemo14.github.io/school-app/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/school-app/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
